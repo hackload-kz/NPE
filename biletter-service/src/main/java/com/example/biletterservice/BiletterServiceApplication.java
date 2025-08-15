43f0cc6c60944f72af120c9a30f36de25adbdddb
azamat <bravlstarstop05@mail.ru>
2025-08-16 00:29:12 +0500

    initial commit

---
+++ biletter-service/src/main/java/com/example/biletterservice/BiletterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.biletterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiletterServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiletterServiceApplication.class, args);
+    }
+
+}
---
+++ biletter-service/src/test/java/com/example/biletterservice/BiletterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.biletterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiletterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
