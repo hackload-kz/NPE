6b3221af5dd8376aeeb79124ca86bd9a22edc463
Rakhimbaev <Rakhimbaev@Bpcbt.com>
2025-08-16 21:32:02 +0500

    Fix review

--- biletter-service-core/src/main/java/com/example/biletterservice/service/UserService.java
+++ biletter-service-core/src/main/java/com/example/biletterservice/service/UserService.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    private final UserRepository repo;
     private final UserRepository userRepository;
 
     @Nullable
